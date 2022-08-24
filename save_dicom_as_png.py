e5e75311f98e9aaefb07d73b819ec05f6c00f802
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2022-08-24 19:42:18 -0300

    FEAT: add subfolders

--- save_dicom_as_png.py
+++ save_dicom_as_png.py
@@ -21,18 +21,32 @@ def check_image_type(folder_4):
     else:
         return  ""
 
+def create_directories(dataset_directory):
+    os.mkdir(dataset_directory)
+    os.mkdir(dataset_directory+"/train")
+    os.mkdir(dataset_directory+"/train/full_mammogram")
+    os.mkdir(dataset_directory+"/train/cropped")
+    os.mkdir(dataset_directory+"/train/ROI_mask")
+    os.mkdir(dataset_directory+"/test")
+    os.mkdir(dataset_directory+"/test/full_mammogram")
+    os.mkdir(dataset_directory+"/test/cropped")
+    os.mkdir(dataset_directory+"/test/ROI_mask")
+    return
+
+
 if __name__ == "__main__":
     """
     Example:
     manifest-1657064287305\CBIS-DDSM\Calc-Test_P_00038_LEFT_CC\08-29-2017-DDSM-NA-96009\1.000000-full mammogram images-63992\1-1.dcm
             level 0     /    level 1      /   level 2              /     level 3            /          level 4       /       level 5
-    """   
+    """
     
+    # Create directory to Dataset
+    dataset_directory = "Dataset-DDSM-png"
+    create_directories(dataset_directory)
+
     for folder_0 in os.listdir(): # manifest-1657064287305 
-        # Create directory to Dataset
-        dataset_directory = "Dataset-DDSM-png"
-        os.mkdir(dataset_directory)
-        if folder_0 != "env-ddsm" and os.path.isdir(folder_0):
+        if folder_0 != "env-dicom" and os.path.isdir(folder_0):
             for folder_1 in os.listdir(folder_0): # CBIS-DDSM
                 path_level1 = f"{folder_0}/{folder_1}"
                 if os.path.isdir(path_level1):
@@ -50,8 +64,9 @@ if __name__ == "__main__":
                                                     path_level5 = f"{folder_0}/{folder_1}/{folder_2}/{folder_3}/{folder_4}/{dcm_file}"
                                                     ds = dcmread(path_level5) # Read file
                                                     type_image = check_image_type(folder_4) # Check type
-                                                    path_image_in_dataset = f"{dataset_directory}/{folder_2}-{type_image}.png"
+                                                    train_or_test = "train" if "Traning" in folder_2 else "test"
+                                                    path_image_in_dataset = f"{dataset_directory}/{train_or_test}/{type_image}/{folder_2}.png"
                                                     cv2.imwrite(path_image_in_dataset, ds.pixel_array) # Write as a png
-                                                    print(f"Write {path_image_in_dataset}") # Dataset-DDSM-png/Mass-Training_P_02079_RIGHT_CC_1-ROI_mask.png                  
+                                                    print(f"Write {path_image_in_dataset}")                  
 
 
