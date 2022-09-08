7e246a4bb7fc8f067928cb9319f6b2ece41f1599
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2022-09-08 00:19:13 -0300

    split in full, cropped and roi

--- save_dicom_as_png.py
+++ save_dicom_as_png.py
@@ -23,14 +23,18 @@ def check_image_type(folder_4):
 
 def create_directories(dataset_directory):
     os.mkdir(dataset_directory)
-    os.mkdir(dataset_directory+"/train")
-    os.mkdir(dataset_directory+"/train/full_mammogram")
-    os.mkdir(dataset_directory+"/train/cropped")
-    os.mkdir(dataset_directory+"/train/ROI_mask")
-    os.mkdir(dataset_directory+"/test")
-    os.mkdir(dataset_directory+"/test/full_mammogram")
-    os.mkdir(dataset_directory+"/test/cropped")
-    os.mkdir(dataset_directory+"/test/ROI_mask")
+    for train_test in ["train","test"]:            
+        path_train_test = dataset_directory+f"/{train_test}"
+        os.mkdir(path_train_test)
+        print(f"Path {path_train_test} created")
+        for calc_mass in ["calc","mass"]:
+            path_calc_mass = dataset_directory+f"/{train_test}/{calc_mass}"
+            os.mkdir(path_calc_mass)
+            print(f"Path {path_calc_mass} created")
+            for full_cropped_roi in ["full_mammogram","cropped","ROI_mask"]:
+                full_path = dataset_directory+f"/{train_test}/{calc_mass}/{full_cropped_roi}" 
+                os.mkdir(full_path)
+                print(f"Path {full_path} created")
     return
 
 
@@ -44,7 +48,8 @@ if __name__ == "__main__":
     # Create directory to Dataset
     dataset_directory = "Dataset-DDSM-png"
     create_directories(dataset_directory)
-
+    print(" -------------------------------------")
+    print("Start Extraction")
     for folder_0 in os.listdir(): # manifest-1657064287305 
         if folder_0 != "env-dicom" and os.path.isdir(folder_0):
             for folder_1 in os.listdir(folder_0): # CBIS-DDSM
@@ -64,9 +69,8 @@ if __name__ == "__main__":
                                                     path_level5 = f"{folder_0}/{folder_1}/{folder_2}/{folder_3}/{folder_4}/{dcm_file}"
                                                     ds = dcmread(path_level5) # Read file
                                                     type_image = check_image_type(folder_4) # Check type
-                                                    train_or_test = "train" if "Traning" in folder_2 else "test"
-                                                    path_image_in_dataset = f"{dataset_directory}/{train_or_test}/{type_image}/{folder_2}.png"
+                                                    train_or_test = "train" if "Training" in folder_2 else "test"
+                                                    calc_or_mass = "calc" if "Calc" in folder_2 else "mass"
+                                                    path_image_in_dataset = f"{dataset_directory}/{train_or_test}/{calc_or_mass}/{type_image}/{folder_2}.png"
                                                     cv2.imwrite(path_image_in_dataset, ds.pixel_array) # Write as a png
-                                                    print(f"Write {path_image_in_dataset}")                  
-
-
+                                                    print(f"Write {path_image_in_dataset}")                                                                  
